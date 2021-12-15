516c7150f1543dc3a0255450368a659d649c503a
michaeljakob <michael@jakob.tv>
2021-12-15 18:49:29 +0100

    fix imprint

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -2,22 +2,20 @@
   <div class="text-left p-8">
     <h1 class="font-bold py-4">Impressum</h1>
     
-    Inhaltlich verantwortlich gem. § 55 Staatsvertrag für Rundfunk und Telemedien (Rundfunk-Staatsvertrag) und Anbieter gem. § 5 Telemediengesetz (TMG) ist:<br>
+    Information Pursuant to § 5 TMG (German Telemedia Act):<br><br>
     <div>
       AlleAktien GmbH<br>
-      Abteilung finfo<br>
+      Department finfo<br>
       Helene-Mayer-Ring 4<br>
-      80809 München<br>
+      80809 Munich<br>
+      Germany
     </div>
     <br><br>
     <div>
-      Benjamin Franzil<br>
+      Benjamin S. Franzil<br>
       benjamin@superfinfo.com<br>
-      +49 170 3141588 (Web-Administrator)
+      +49 170 3141588
     </div>
-
-
-
 
     <div class="my-24">
       <nuxt-link
